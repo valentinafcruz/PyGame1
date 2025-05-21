2b51f6aa1b2a54c6468406b4da8ae849f36324d3
valentinafcruz <valentinafcruz@outlook.com>
2025-05-21 17:40:16 -0300

    level2

---
+++ paredes.py
@@ -0,0 +1,29 @@
+Mapa Level2
+    barreiras.clear()
+    #Paredes - Labirinto 1
+    #cantos
+    draw_wall1(0,0,50,800)
+    draw_wall1(0,0,800,50)
+    draw_wall1(0,750,800,50)
+    draw_wall1(750,0,50,800)
+    
+    draw_wall1(50,100,150,50) #1
+    draw_wall1(250,50,50,600) #2
+    draw_wall1(100, 200, 150, 50) #3
+    draw_wall1(50, 300, 150, 50) #4
+    draw_wall1(100, 450, 150, 50) #5
+    draw_wall1(50, 600, 150, 50) #6
+    draw_wall1(300, 700, 500, 500) #8
+    draw_wall1(300, 600, 300, 50) #9
+    draw_wall1(350, 450, 300, 50) #10
+    draw_wall1(600, 500, 50, 50) #11
+    draw_wall1(300, 250, 300, 50) #12
+    draw_wall1(300, 300, 50, 50) #13
+    draw_wall1(450, 350, 50, 100) #14
+    draw_wall1(350, 100, 300, 50) #15
+    draw_wall1(600, 150, 50, 50) #16
+    draw_wall1(650,100, 50, 700) #7
+
+
+
+
